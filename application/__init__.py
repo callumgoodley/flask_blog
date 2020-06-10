74135bac15f51c203e64585cd169782e61c2993e
callumgoodley <callumgoodley@btinternet.com>
2020-06-10 14:36:36 +0000

    login functionality added along with changing account info and deleting account as well as posts from that account

--- application/__init__.py
+++ application/__init__.py
@@ -2,13 +2,17 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import getenv
 from flask_bcrypt import Bcrypt
-
+from flask_login import LoginManager
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = str(getenv('FLASK_BLOG_URI'))
 app.config['SECRET_KEY'] = getenv('MY_SECRET_KEY')
 
 db = SQLAlchemy(app)
+
 bcrypt = Bcrypt(app)
+
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 
 from application import routes
